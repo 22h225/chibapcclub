b358ad76a0469b65134146448aee8e8ce89b368f
22h225 <22h225@kenchiba.net>
2023-09-22 23:55:21 +0900

    css kanrenn no tuika to ho-mupe-zi sukosi kaita

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,17 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    ssr: {
+      noExternal: ['normalize.css']
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "src/styles/vars.scss";'
+        }
+      }
+    }
+  }
+});
